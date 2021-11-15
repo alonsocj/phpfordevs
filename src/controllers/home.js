a1d6d301c04ebb49c9a7738c3225fc4a73d59897
alonsocj <cornejo.aj98@hotmail.com>
2021-11-15 12:09:10 -0600

    se agregaron comentarios a los archivos a modificar

--- src/controllers/home.js
+++ src/controllers/home.js
@@ -1,6 +1,13 @@
 import { conexion } from "../database";
+
+// se define una funcion que en este caso solicita la informacion a la base de datos
+//esta funcion automaticamente se exporta
 export const ejemplo = async (req, res) => {
-  const { rows } = await conexion.query("SELECT * FROM tabla");
-  console.log(rows);
+  // esta seria la forma en la que se solicitaria informacion a la base de datos
+  /* const { rows } = await conexion.query("SELECT * FROM tabla");
+  console.log(rows); */
+  //se extraeria la informacion en la variable rows (esta definida asi desde la consulta)
+  const rows = ''
+  //se envia un objeto a la plantilla (en este caso la plantilla index)
   res.render("index", { data: rows });
 };
--- src/routes/home.js
+++ src/routes/home.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { ejemplo } from "../controllers/home";
 const router = Router();
 
+// aqui de esta manera se define una ruta y se llama la funcion de ejemplo que se determino en el controlador
 router.get("/", ejemplo);
 
 export default router;
