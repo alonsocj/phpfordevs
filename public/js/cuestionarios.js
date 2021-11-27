3dd256defdeef6dd8ca149b51d141fcd72898b94
GusPD <86135999+GusPD@users.noreply.github.com>
2021-11-26 18:48:26 -0600

    cuestionarios

--- public/js/cuestionarios.js
+++ public/js/cuestionarios.js
@@ -1,1 +1,14 @@
-f
+window.addEventListener("load", function(){
+	contarPreguntasBasico();
+    contarPreguntasIntermedio();
+    contarPreguntasAvanzado();
+});
+function contarPreguntasBasico(){
+    document.getElementById("cantBasico").innerHTML="Preguntas:"+ questionsB.length;
+}
+function contarPreguntasIntermedio(){
+    document.getElementById("cantIntermedio").innerHTML="Preguntas:"+ questionsI.length;
+}
+function contarPreguntasAvanzado(){
+    document.getElementById("cantAvanzado").innerHTML="Preguntas:"+ questionsA.length;
+}
