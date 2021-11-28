d408628b8809c3b435081736a9c5532969325866
alonsocj <cornejo.aj98@hotmail.com>
2021-11-27 19:04:39 -0600

    modificacion en video

--- src/controllers/videos.js
+++ src/controllers/videos.js
@@ -1,7 +1,10 @@
 import { conexion } from "../database";
 
 export const getVideos = async (req, res) => {
-  const { rows } = await conexion.query("SELECT * FROM video");
+  const { rows } = await conexion.query(
+    "SELECT * FROM video WHERE id_curso = $1",
+    [req.paramas.id]
+  );
   console.log(rows);
   res.render("videos", { data: rows });
 };
