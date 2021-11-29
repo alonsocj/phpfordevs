f0081d6e15bf1154a44497c08741871851becd40
alonsocj <cornejo.aj98@hotmail.com>
2021-11-29 00:12:14 -0600

    estilos de lista de videos agregados

--- src/controllers/videos.js
+++ src/controllers/videos.js
@@ -4,17 +4,26 @@ export const cursos = async (req, res) => {
   res.render("cursos");
 };
 
+const getCurso = async (id) => {
+  const { rows } = await conexion.query(
+    "SELECT * FROM curso WHERE id_curso = $1",
+    [id]
+  );
+  return rows;
+};
+
 export const getVideos = async (req, res) => {
   const rows = await getListVideos(req.params.id);
-  res.render("videos", { data: rows });
+  const curso = await getCurso(req.params.id);
+  res.render("videos", { data: rows, curso: curso });
 };
 export const watchVideo = async (req, res) => {
   const { rows } = await conexion.query(
     "SELECT * FROM video WHERE id_curso = $1 AND cod = $2",
     [req.params.id, req.params.cod]
   );
-  console.log(rows);
-  res.render("vervideo", { video: rows });
+  const lista = await getListVideos(req.params.id);
+  res.render("vervideo", { video: rows, lista: lista });
 };
 const getListVideos = async (id) => {
   const { rows } = await conexion.query(
