f98757697711da57964e2ba591175244e7e7a7cd
alonsocj <cornejo.aj98@hotmail.com>
2021-11-30 02:09:11 -0600

    algunos estilos al ver un video

---
+++ public/js/videolist.js
@@ -0,0 +1,8 @@
+let sidebar = document.querySelector(".sidebar-videos");
+let sidebarBtn = document.querySelector(".home-content");
+let container = document.querySelector(".container-watch");
+
+sidebarBtn.addEventListener("click", () => {
+  sidebar.classList.toggle("close");
+  
+});
