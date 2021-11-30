7a6c4258d27184e21ae721cd53f9829dbcccbb5c
alonsocj <cornejo.aj98@hotmail.com>
2021-11-30 14:57:24 -0600

    Nueva version del script

--- scripts/phpfordev.sql
+++ scripts/phpfordev.sql
@@ -1,8 +1,10 @@
 /*==============================================================*/
 /* DBMS name:      PostgreSQL 8                                 */
-/* Created on:     11/29/2021 1:50:38 PM                        */
+/* Created on:     11/30/2021 12:32:43 PM                       */
 /*==============================================================*/
 
+
+drop index RELATIONSHIP_7_FK;
 
 drop index RELATIONSHIP_3_FK;
 
@@ -20,6 +22,8 @@ drop index EJEMPLO_PK;
 
 drop table EJEMPLO;
 
+drop index RELATIONSHIP_6_FK;
+
 drop index FORO_PK;
 
 drop table FORO;
@@ -30,11 +34,19 @@ drop index RECURSOS_PK;
 
 drop table RECURSOS;
 
+drop index RELATIONSHIP_9_FK;
+
 drop index RELATIONSHIP_4_FK;
 
 drop index RESPUESTA_PK;
 
 drop table RESPUESTA;
+
+drop index USUARIO_PK;
+
+drop table USUARIO;
+
+drop index RELATIONSHIP_8_FK;
 
 drop index RELATIONSHIP_1_FK;
 
@@ -48,7 +60,7 @@ drop table VIDEO;
 create table COMENTARIO (
    ID_COMENT            SERIAL               not null,
    COD                  INT4                 null,
-   USUARIO              VARCHAR(150)         not null,
+   ID_USER              INT4                 null,
    OPINION              VARCHAR(250)         not null,
    MEGUSTA              INT4                 not null,
    NOMEGUSTA            INT4                 not null,
@@ -67,6 +79,13 @@ ID_COMENT
 /*==============================================================*/
 create  index RELATIONSHIP_3_FK on COMENTARIO (
 COD
+);
+
+/*==============================================================*/
+/* Index: RELATIONSHIP_7_FK                                     */
+/*==============================================================*/
+create  index RELATIONSHIP_7_FK on COMENTARIO (
+ID_USER
 );
 
 /*==============================================================*/
@@ -119,9 +138,9 @@ COD
 /*==============================================================*/
 create table FORO (
    ID_FORO              SERIAL               not null,
+   ID_USER              INT4                 null,
    DESCRIPCION          VARCHAR(250)         not null,
    TITFORO              VARCHAR(150)         not null,
-   USER_FORO            VARCHAR(150)         not null,
    FECHA_PUB            VARCHAR(50)          not null,
    constraint PK_FORO primary key (ID_FORO)
 );
@@ -131,6 +150,13 @@ create table FORO (
 /*==============================================================*/
 create unique index FORO_PK on FORO (
 ID_FORO
+);
+
+/*==============================================================*/
+/* Index: RELATIONSHIP_6_FK                                     */
+/*==============================================================*/
+create  index RELATIONSHIP_6_FK on FORO (
+ID_USER
 );
 
 /*==============================================================*/
@@ -164,10 +190,10 @@ COD
 create table RESPUESTA (
    ID_RESPUESTA         SERIAL               not null,
    ID_FORO              INT4                 null,
+   ID_USER              INT4                 null,
    DESCRIPCION_RES      VARCHAR(250)         not null,
    AYUDA                INT4                 not null,
    NOAYUDA              INT4                 not null,
-   USUARIO_RES          VARCHAR(150)         not null,
    FECHA_COMENT         VARCHAR(50)          not null,
    constraint PK_RESPUESTA primary key (ID_RESPUESTA)
 );
@@ -187,15 +213,41 @@ ID_FORO
 );
 
 /*==============================================================*/
+/* Index: RELATIONSHIP_9_FK                                     */
+/*==============================================================*/
+create  index RELATIONSHIP_9_FK on RESPUESTA (
+ID_USER
+);
+
+/*==============================================================*/
+/* Table: USUARIO                                               */
+/*==============================================================*/
+create table USUARIO (
+   ID_USER              SERIAL               not null,
+   USERNAME             VARCHAR(255)         not null,
+   PASSWORD             VARCHAR(255)         not null,
+   constraint PK_USUARIO primary key (ID_USER)
+);
+
+/*==============================================================*/
+/* Index: USUARIO_PK                                            */
+/*==============================================================*/
+create unique index USUARIO_PK on USUARIO (
+ID_USER
+);
+
+/*==============================================================*/
 /* Table: VIDEO                                                 */
 /*==============================================================*/
 create table VIDEO (
    COD                  SERIAL               not null,
    ID_CURSO             INT4                 null,
+   ID_USER              INT4                 null,
    TITULO               VARCHAR(150)         not null,
    URL                  VARCHAR(150)         not null,
    LIKES                INT4                 not null,
    DISLIKES             INT4                 not null,
+   DESCRIPCION_VIDEO    VARCHAR(150)         not null,
    constraint PK_VIDEO primary key (COD)
 );
 
@@ -213,14 +265,31 @@ create  index RELATIONSHIP_1_FK on VIDEO (
 ID_CURSO
 );
 
+/*==============================================================*/
+/* Index: RELATIONSHIP_8_FK                                     */
+/*==============================================================*/
+create  index RELATIONSHIP_8_FK on VIDEO (
+ID_USER
+);
+
 alter table COMENTARIO
    add constraint FK_COMENTAR_RELATIONS_VIDEO foreign key (COD)
       references VIDEO (COD)
       on delete restrict on update restrict;
 
+alter table COMENTARIO
+   add constraint FK_COMENTAR_RELATIONS_USUARIO foreign key (ID_USER)
+      references USUARIO (ID_USER)
+      on delete restrict on update restrict;
+
 alter table EJEMPLO
    add constraint FK_EJEMPLO_RELATIONS_VIDEO foreign key (COD)
       references VIDEO (COD)
+      on delete restrict on update restrict;
+
+alter table FORO
+   add constraint FK_FORO_RELATIONS_USUARIO foreign key (ID_USER)
+      references USUARIO (ID_USER)
       on delete restrict on update restrict;
 
 alter table RECURSOS
@@ -233,8 +302,18 @@ alter table RESPUESTA
       references FORO (ID_FORO)
       on delete restrict on update restrict;
 
+alter table RESPUESTA
+   add constraint FK_RESPUEST_RELATIONS_USUARIO foreign key (ID_USER)
+      references USUARIO (ID_USER)
+      on delete restrict on update restrict;
+
 alter table VIDEO
    add constraint FK_VIDEO_RELATIONS_CURSO foreign key (ID_CURSO)
       references CURSO (ID_CURSO)
+      on delete restrict on update restrict;
+
+alter table VIDEO
+   add constraint FK_VIDEO_RELATIONS_USUARIO foreign key (ID_USER)
+      references USUARIO (ID_USER)
       on delete restrict on update restrict;
 
