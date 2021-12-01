2246d24330f8fc5aa681fad1dddbf745374f1cf0
alonsocj <cornejo.aj98@hotmail.com>
2021-11-30 23:38:59 -0600

    js modificado en el video

--- public/js/videolist.js
+++ public/js/videolist.js
@@ -1,8 +1,6 @@
 let sidebar = document.querySelector(".sidebar-videos");
 let sidebarBtn = document.querySelector(".home-content");
-let container = document.querySelector(".container-watch");
 
 sidebarBtn.addEventListener("click", () => {
   sidebar.classList.toggle("close");
-  
 });
