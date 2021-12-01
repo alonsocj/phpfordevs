b7c28cc6a28d9450c6aa6d487fc92402bac52fb1
alonsocj <cornejo.aj98@hotmail.com>
2021-12-01 16:08:45 -0600

    funcionalidad de comentar un video agregada

--- src/controllers/videos.js
+++ src/controllers/videos.js
@@ -19,17 +19,63 @@ export const getVideos = async (req, res) => {
   res.render("videos", { data: rows, curso: curso });
 };
 export const watchVideo = async (req, res) => {
+  const codvid = req.params.cod;
   const { rows } = await conexion.query(
     "SELECT * FROM video WHERE id_curso = $1 AND cod = $2",
-    [req.params.id, req.params.cod]
+    [req.params.id, codvid]
   );
+  const recurso = await getRecurso(codvid);
+
   const lista = await getListVideos(req.params.id);
-  res.render("vervideo", { video: rows, lista: lista });
+  const comments = await getComments(codvid);
+  const users = [];
+  for (let i = 0; i < comments.length; i++) {
+    users.push(await getUser(comments[i].id_user));
+  }
+  res.render("vervideo", {
+    video: rows,
+    lista: lista,
+    login: req.session.loggedin,
+    comments: comments,
+    users: users,
+    recurso: recurso,
+  });
+};
+export const postComments = async (req, res) => {
+  const idcurso = req.params.id;
+  const codvideo = req.params.cod;
+  await conexion.query(
+    "INSERT INTO comentario (cod,id_user,opinion,megusta) VALUES($1,$2,$3,$4)",
+    [codvideo, req.session.name, req.body.comment, 0]
+  );
+  res.redirect("/cursos/" + idcurso + "/videos/" + codvideo);
 };
 const getListVideos = async (id) => {
   const { rows } = await conexion.query(
     "SELECT * FROM video WHERE id_curso = $1",
     [id]
   );
+  return rows;
+};
+const getUser = async (id) => {
+  const { rows } = await conexion.query(
+    "SELECT * FROM usuario WHERE id_user=$1",
+    [id]
+  );
+  return rows;
+};
+const getComments = async (id_coment) => {
+  const { rows } = await conexion.query(
+    "SELECT * FROM comentario WHERE cod = $1",
+    [id_coment]
+  );
+  return rows;
+};
+const getRecurso = async (cod) => {
+  const { rows } = await conexion.query(
+    "SELECT * FROM recursos WHERE cod = $1",
+    [cod]
+  );
+  console.log(rows);
   return rows;
 };
--- src/routes/videos.js
+++ src/routes/videos.js
@@ -1,9 +1,15 @@
 import { Router } from "express";
-import { cursos, watchVideo, getVideos } from "../controllers/videos";
+import {
+  cursos,
+  watchVideo,
+  getVideos,
+  postComments,
+} from "../controllers/videos";
 const router = Router();
 
 router.get("/cursos/", cursos);
 router.get("/cursos/:id/videos", getVideos);
 router.get("/cursos/:id/videos/:cod", watchVideo);
+router.post("/cursos/:id/videos/:cod", postComments);
 
 export default router;
