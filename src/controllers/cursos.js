a26d1170c8fa7b8455daad6ae72b50e8455eb83f
GusPD <86135999+GusPD@users.noreply.github.com>
2021-12-02 21:13:37 -0600

    cursos

--- src/controllers/cursos.js
+++ src/controllers/cursos.js
@@ -3,11 +3,19 @@ import { conexion } from "../database";
 
 export const getCursos = async (req, res) => {
   const { rows } = await conexion.query("SELECT * FROM curso");
+  const { rowsRecursos } = await conexion.query("SELECT * FROM recursos");
+  const { rowsVideos } = await conexion.query("SELECT * FROM video");
   console.log(rows);
+  console.log(rowsRecursos);
+  console.log(rowsVideos);
+  
   //Pintar en pantalla
   res.render("cursos", {
     data: rows,
     login: req.session.loggedin,
     name: req.session.name,
+    recurso:rowsRecursos,
+    video:rowsVideos,
   });
 };
+
