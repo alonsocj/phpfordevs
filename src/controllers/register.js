5fdb454af261227c0440da248f0d053460e02ad3
alonsocj <cornejo.aj98@hotmail.com>
2021-12-03 15:11:52 -0600

    Validacion a la hora de registar usuarios

--- src/controllers/register.js
+++ src/controllers/register.js
@@ -9,24 +9,36 @@ export const register = async (req, res) => {
 export const registerPost = async (req, res) => {
   const user = req.body.user;
   const pass = req.body.pass;
-  const passhash = await bcryptjs.hash(pass, 8);
-  await conexion.query(
-    "INSERT INTO usuario (username,password) VALUES ($1,$2)",
-    [user, passhash],
-    async (error, results) => {
-      if (error) {
-        console.log(error);
-      } else {
-        res.render("register", {
-          alert: true,
-          alertTitle: "Nuevo Usuario!",
-          alertMessage: "Registro Exitoso",
-          alertIcon: "success",
-          showConfirmButton: false,
-          time: 1500,
-          ruta: "login",
-        });
+  if (user == "" || pass == "") {
+    res.render("register", {
+      alert: true,
+      alertTitle: "Error al Registrar Usuario",
+      alertMessage: "No puede registrar si tiene espacios vacios",
+      alertIcon: "error",
+      showConfirmButton: true,
+      time: false,
+      ruta: "register",
+    });
+  } else {
+    const passhash = await bcryptjs.hash(pass, 8);
+    await conexion.query(
+      "INSERT INTO usuario (username,password) VALUES ($1,$2)",
+      [user, passhash],
+      async (error, results) => {
+        if (error) {
+          console.log(error);
+        } else {
+          res.render("register", {
+            alert: true,
+            alertTitle: "Nuevo Usuario!",
+            alertMessage: "Registro Exitoso",
+            alertIcon: "success",
+            showConfirmButton: false,
+            time: 1500,
+            ruta: "login",
+          });
+        }
       }
-    }
-  );
+    );
+  }
 };
