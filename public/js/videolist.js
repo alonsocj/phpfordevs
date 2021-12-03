8d5addc0bb902a51bd7df49225593e2242612087
alonsocj <cornejo.aj98@hotmail.com>
2021-12-03 12:33:51 -0600

    validaciones a la hora de escribir un comentario

--- public/js/videolist.js
+++ public/js/videolist.js
@@ -4,3 +4,40 @@ let sidebarBtn = document.querySelector(".home-content");
 sidebarBtn.addEventListener("click", () => {
   sidebar.classList.toggle("close");
 });
+const maxPermitted = 250;
+// La varible de control
+let validArea = true;
+
+const area = document.querySelector(".formatextarea");
+const submit = document.querySelector(".btnComment");
+const error = document.querySelector("#area-error");
+
+submit.addEventListener(`click`, () => {
+  // Quizás sería más conveniente darle el
+  // atributo 'disabled' true/false
+  // en los métodos 'handleValid' y
+  // 'handleInvalid'
+  if (validArea) console.log(`valid`);
+  else console.warn(`invalid`);
+});
+
+// Nos suscribimos al evento 'input'
+// del area, que se disparará cada
+// vez que cambie el valor del textarea
+area.addEventListener(`input`, () => {
+  const { value } = area;
+
+  value.length >= maxPermitted ? handleInvalid() : handleValid();
+});
+
+// Mostramos el texto de error
+const handleInvalid = () => {
+  validArea = false;
+  error.style.display = `block`;
+};
+
+// Ocultamos el bloque de error
+const handleValid = () => {
+  validArea = true;
+  error.style.display = `none`;
+};
