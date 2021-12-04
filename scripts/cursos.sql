609c504e854723224c4239e414ec92727fe508f2
alonsocj <cornejo.aj98@hotmail.com>
2021-12-03 18:41:04 -0600

    sentencias sql de videos y cursos, activacion del ssl

---
+++ scripts/cursos.sql
@@ -0,0 +1,48 @@
+INSERT INTO
+  curso (contenido, tiempo, titulo_curso)
+VALUES
+  (
+    'Conexión de base de datos. Insertar en base de datos. Modificar en la base de datos. Eliminar en la base de datos.',
+    '01:34:01',
+    'Base de datos con PHP'
+  );
+INSERT INTO
+  curso (contenido, tiempo, titulo_curso)
+VALUES
+  (
+    'Expresiones regulares. Get y post. ',
+    '00:32:13',
+    'Creación de Formularios en PHP'
+  );
+INSERT INTO
+  curso (contenido, tiempo, titulo_curso)
+VALUES
+  (
+    'Explicación sobre API. Consumo de una API. ',
+    '00:39:37',
+    'Consumir API en PHP'
+  );
+INSERT INTO
+  curso (contenido, tiempo, titulo_curso)
+VALUES
+  (
+    'Sesiones y Manejo de Cookies.',
+    '00:10:49',
+    'Sesiones y Manejo de Cookies'
+  );
+INSERT INTO
+  curso (contenido, tiempo, titulo_curso)
+VALUES
+  (
+    'CRUD Con PHP.',
+    '00:23:21',
+    'Creación de un Proyecto [CRUD En PHP]'
+  );
+INSERT INTO
+  curso (contenido, tiempo, titulo_curso)
+VALUES
+  (
+    'Creación de un Chat con WebSockets y Php.',
+    '00:23:21',
+    'WebSockets en PHP'
+  );
---
+++ scripts/videos.sql
@@ -0,0 +1,238 @@
+INSERT INTO
+  video (id_curso, titulo, url, likes, descripcion_video)
+VALUES
+  (
+    1,
+    'Introducción del Curso',
+    'https://www.youtube.com/embed/sLehh23iPCU',
+    0,
+    'Muestra una descripción general de los objetivos de la página web y el programa de clases a impartir.'
+  );
+INSERT INTO
+  video (id_curso, titulo, url, likes, descripcion_video)
+VALUES
+  (
+    2,
+    'Instalación de PHP',
+    'https://www.youtube.com/embed/2_WT8GlBDbE',
+    0,
+    'Video explicativo para instalar y configurar el entorno de trabajo de PHP utilizando XAMPP en Windows.'
+  );
+  /* falta 
+                                                        aaaaa
+                                                        aaaaa
+                                                      
+                                                        aaaaa
+                                                        
+                                                        
+                                                        linux */
+INSERT INTO
+  video (id_curso, titulo, url, likes, descripcion_video)
+VALUES
+  (
+    4,
+    'Tipos de datos.',
+    'https://www.youtube.com/embed/AseKI3tX9wo',
+    0,
+    'Video orientado a explicar los diferentes tipos de datos en PHP así como su declaración.'
+  );
+INSERT INTO
+  video (id_curso, titulo, url, likes, descripcion_video)
+VALUES
+  (
+    4,
+    'Sintaxis (Creación de funciones, variables)',
+    'https://www.youtube.com/embed/nQQNekVYPzI',
+    0,
+    'Video orientado a explicar la sintaxis básica de PHP necesaria a la hora de declarar funciones y variables.'
+  );
+INSERT INTO
+  video (id_curso, titulo, url, likes, descripcion_video)
+VALUES
+  (
+    4,
+    'Estructuras condicionales',
+    'https://www.youtube.com/embed/WECOPY4TQIs',
+    0,
+    'En este video se explica acerca de la declaración y uso de las estructuras condicionales mediante un ejemplo de aplicación.'
+  );
+INSERT INTO
+  video (id_curso, titulo, url, likes, descripcion_video)
+VALUES
+  (
+    4,
+    'Ciclos',
+    'https://www.youtube.com/embed/3zoFwfxIeSk',
+    0,
+    'En este video se explica acerca de las estructuras While y For, así como un breve repaso acerca de las estructuras condicionales para mayor comprensión.'
+  );
+INSERT INTO
+  video (id_curso, titulo, url, likes, descripcion_video)
+VALUES
+  (
+    4,
+    'Manejo de Errores',
+    'https://www.youtube.com/embed/ajkLkoag-DE',
+    0,
+    'Video orientado a explicar los métodos para manejar y controlar los errores en PHP.'
+  );
+INSERT INTO
+  video (id_curso, titulo, url, likes, descripcion_video)
+VALUES
+  (
+    4,
+    'Arreglos y cadenas en PHP.',
+    'https://www.youtube.com/embed/7U7KCdyDww4',
+    0,
+    'Video sobre arreglos y cadenas, su sintaxis para su declaración, acceso a sus valores y uso en el lenguaje PHP.'
+  );
+INSERT INTO
+  video (id_curso, titulo, url, likes, descripcion_video)
+VALUES
+  (
+    5,
+    'Creación de objetos.',
+    'https://www.youtube.com/embed/cptowAYei20',
+    0,
+    'Video introductorio a PHP orientado a objetos, para aprender a trabajar con clases, métodos y objetos. '
+  );
+INSERT INTO
+  video (id_curso, titulo, url, likes, descripcion_video)
+VALUES
+  (
+    5,
+    'Clases y métodos abstractos.',
+    'https://www.youtube.com/embed/XxxCU3kqAMg',
+    0,
+    'Video explicativo con respecto a la aplicación y funcionamiento de las clases y métodos abstractos en PHP.'
+  );
+INSERT INTO
+  video (id_curso, titulo, url, likes, descripcion_video)
+VALUES
+  (
+    5,
+    'Intefaces',
+    'https://www.youtube.com/embed/XxxCU3kqAMg',
+    0,
+    'Video sobre constructores y destructores en el lenguaje de programación PHP en donde se explica cómo funcionan y cuál es su aplicación.'
+  );
+INSERT INTO
+  video (id_curso, titulo, url, likes, descripcion_video)
+VALUES
+  (
+    5,
+    'Herencia',
+    'https://www.youtube.com/embed/ct7pReCvz0g',
+    0,
+    'Video orientado a explicar la implementación de la herencia en PHP y las utilidades que esta proporciona al trabajar con clases.'
+  );
+INSERT INTO
+  video (id_curso, titulo, url, likes, descripcion_video)
+VALUES
+  (
+    6,
+    'Conexión de base de datos.',
+    'https://www.youtube.com/embed/K5ioEZ6m8Js',
+    0,
+    'Vídeo explicativo sobre el proceso a seguir para establecer la conexión con la base de datos de un proyecto en PHP.'
+  );
+INSERT INTO
+  video (id_curso, titulo, url, likes, descripcion_video)
+VALUES
+  (
+    6,
+    'Insertar en base de datos.',
+    'https://www.youtube.com/embed/Jasn-LiCh58',
+    0,
+    'Vídeo explicativo sobre la inserción de información en una base de datos a través de un formulario en PHP.'
+  );
+INSERT INTO
+  video (id_curso, titulo, url, likes, descripcion_video)
+VALUES
+  (
+    6,
+    'Modificar en la base de datos',
+    'https://www.youtube.com/embed/XCChdZ17Zgk',
+    0,
+    'Vídeo explicativo sobre la modificación de información en una base de datos a través de un formulario en PHP.'
+  );
+INSERT INTO
+  video (id_curso, titulo, url, likes, descripcion_video)
+VALUES
+  (
+    6,
+    'Eliminar en la base de datos',
+    'https://www.youtube.com/embed/-_i8qFe8Kbk',
+    0,
+    'Vídeo explicativo sobre la eliminación de información en una base de datos a través de un formulario en PHP.'
+  );
+INSERT INTO
+  video (id_curso, titulo, url, likes, descripcion_video)
+VALUES
+  (
+    7,
+    'Expresiones regulares',
+    'https://www.youtube.com/embed/4q0A_r374no',
+    0,
+    'Recurso audiovisual que proporciona la forma de uso de las expresiones regulares cuando se programa con PHP.'
+  );
+INSERT INTO
+  video (id_curso, titulo, url, likes, descripcion_video)
+VALUES
+  (
+    7,
+    'Get y post',
+    'https://www.youtube.com/embed/e2R9uvjKW5g',
+    0,
+    'Video que muestra cómo usar los métodos POST y GET en los formularios de html, y las características de cómo trabajan con el lenguaje PHP.'
+  );
+INSERT INTO
+  video (id_curso, titulo, url, likes, descripcion_video)
+VALUES
+  (
+    8,
+    'Explicación sobre API',
+    'https://www.youtube.com/embed/QsrWtqnQGMc',
+    0,
+    'Video introductorio que habla acerca de las API, los usos generales que se le pueden dar así como un ejemplo de API.'
+  );
+INSERT INTO
+  video (id_curso, titulo, url, likes, descripcion_video)
+VALUES
+  (
+    8,
+    'Consumo de una API',
+    'https://www.youtube.com/embed/T6kvsfehsMM',
+    0,
+    'Video que muestra el consumo de una API en este caso de Google Maps, a su vez se hace el uso de HTML, JSON y PHP como herramientas para su realización.'
+  );
+INSERT INTO
+  video (id_curso, titulo, url, likes, descripcion_video)
+VALUES
+  (
+    9,
+    'Sesiones y Manejo de Cookies',
+    'https://www.youtube.com/embed/dxus8CUf3cI',
+    0,
+    'Vídeo explicativo del proceso a seguir para incorporar sesiones y manejo de cookies en un proyecto en HDP.'
+  );
+INSERT INTO
+  video (id_curso, titulo, url, likes, descripcion_video)
+VALUES
+  (
+    10,
+    'CRUD Con PHP',
+    'https://www.youtube.com/embed/6KKIP3HmCdo',
+    0,
+    'En este video se explica como elaborar un CRUD con PHP'
+  );
+INSERT INTO
+  video (id_curso, titulo, url, likes, descripcion_video)
+VALUES
+  (
+    11,
+    'Creación de un Chat con WebSockets y Php',
+    'https://www.youtube.com/embed/BhL_qL3MLJg',
+    0,
+    'Se explica la realización de un Chat en tiempo real mediante la utilización de PHP, RATCHET y JQUERY'
+  );
