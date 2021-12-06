b5dc3f5a18b1a3b57a05e895cb1bb79ecfa0b0a6
alonsocj <cornejo.aj98@hotmail.com>
2021-12-06 11:40:14 -0600

    script para agregar recursos

---
+++ scripts/recursos.sql
@@ -0,0 +1,88 @@
+INSERT INTO
+  recursos (cod, url_recurso, nombre_recurso)
+VALUES
+  (
+    3,
+    'https://drive.google.com/file/d/1lbtCFJvdItnjKPEfNbiu6LE82jkGxhrx/view?usp=sharing',
+    'Manual de Instalación de PHP en Windows'
+  );
+INSERT INTO
+  recursos (cod, url_recurso, nombre_recurso)
+VALUES
+  (
+    23,
+    'https://drive.google.com/file/d/1MYRvJHcMMFV6O2EB08vLnE2-Y87mIjsG/view?usp=sharing',
+    'Manual de Instalación de PHP en Linux'
+  );
+INSERT INTO
+  recursos (cod, url_recurso, nombre_recurso)
+VALUES
+  (
+    25,
+    'https://drive.google.com/file/d/1lN_Q46hkhupoVzbo5wNKE1vRa54oDYDj/view?usp=sharing',
+    'Guia extra de Tipos de datos'
+  );
+INSERT INTO
+  recursos (cod, url_recurso, nombre_recurso)
+VALUES
+  (
+    32,
+    'https://drive.google.com/file/d/176pwuBUqgsI32UmXXkwYOSZy5o1kIqX8/view?usp=sharing',
+    'Guia de Programación Orientada a Objetos en PHP'
+  );
+INSERT INTO
+  recursos (cod, url_recurso, nombre_recurso)
+VALUES
+  (
+    33,
+    'https://drive.google.com/file/d/176pwuBUqgsI32UmXXkwYOSZy5o1kIqX8/view?usp=sharing',
+    'Guia de Programación Orientada a Objetos en PHP'
+  );
+INSERT INTO
+  recursos (cod, url_recurso, nombre_recurso)
+VALUES
+  (
+    35,
+    'https://drive.google.com/file/d/176pwuBUqgsI32UmXXkwYOSZy5o1kIqX8/view?usp=sharing',
+    'Guia de Programación Orientada a Objetos en PHP'
+  );
+INSERT INTO
+  recursos (cod, url_recurso, nombre_recurso)
+VALUES
+  (
+    13,
+    'https://drive.google.com/file/d/1bR1_ziEWnE3K0Zx6D4y_axIzOwEx9rxf/view?usp=sharing',
+    'Guia de Base de datos con PHP'
+  );
+INSERT INTO
+  recursos (cod, url_recurso, nombre_recurso)
+VALUES
+  (
+    14,
+    'https://drive.google.com/file/d/1bR1_ziEWnE3K0Zx6D4y_axIzOwEx9rxf/view?usp=sharing',
+    'Guia de Base de datos con PHP'
+  );
+INSERT INTO
+  recursos (cod, url_recurso, nombre_recurso)
+VALUES
+  (
+    39,
+    'https://drive.google.com/file/d/1p7sBX17hZml9VT3xNWnv6mWM-IAzyXfo/view?usp=sharing',
+    'Manual Consumo de API en PHP'
+  );
+INSERT INTO
+  recursos (cod, url_recurso, nombre_recurso)
+VALUES
+  (
+    21,
+    'https://drive.google.com/file/d/1-M2fXTGTuzla3XQTd7ojy1m2AlWHFyPv/view?usp=sharing',
+    'Manual de Creación de un Proyecto [CRUD En PHP]'
+  );
+INSERT INTO
+  recursos (cod, url_recurso, nombre_recurso)
+VALUES
+  (
+    22,
+    'https://drive.google.com/file/d/123IljxRdox7KCa-EErpMH4KeEIAaUgeA/view?usp=sharing',
+    'Guia sobre WebSockets en PHP'
+  );
